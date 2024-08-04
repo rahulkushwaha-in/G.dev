bd700e53e78d48bc8761bd78db006711d23cd5e0
rahulkushwaha-in <awesomerahul.vl@gmail.com>
2024-08-04 19:11:33 +0530

    favicon added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 // import component from "../components/component/components.jsx"
 import { Landing } from "@/components/Landing";
+<link rel="icon" href="/favicon.ico" sizes="any" />
 
 export default function Home() {
   return (
